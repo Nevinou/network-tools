125c4177b75607f09e6ff40bf1a63834cfc5484c
Nevinou <neyhou@christophelouer.fr>
2025-09-25 13:35:24 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+REPO_BASE="https://raw.githubusercontent.com/network-tools/new/main"
+DOSSIER_INSTALL="/usr/local/bin"
+
+TOOLS=("DHCPkea" "DNSbind" "apache")
+
+echo "[*] Installation des script réseau dans le répertoir $DOSSIER_INSTALL"
+
+for tool in "${TOOLS[@]}";do
+    echo "  ↪ Téléchargement de $tool..."
+    curl -sSL "$REPO_BASE/$tool.sh" -o "$tool"
+    chmod +x "$tool"
+    echo "  ↪ Installation dans $INSTALL_DIR..."
+    mv "$tool" "$INSTALL_DIR/$tool"
+done
+
+echo "[✔] Tous les outils ont été installés avec succès."
