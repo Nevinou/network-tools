ee1e766f1d79050eb1fb8466a9551a11e3344af9
zz-ship-it <103956667+zz-ship-it@users.noreply.github.com>
2025-09-26 11:31:58 +0200

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -2,13 +2,9 @@
 set -e
 #comm
 # Variables (mets la bonne version/build depuis le site Splunk)
-VERSION="9.2.1"
-BUILD="<build>"   # à remplacer par le vrai numéro de build
 
-URL="https://download.splunk.com/products/splunk/releases/$VERSION/linux/splunk-${VERSION}-${BUILD}-Linux-x86_64.deb"
+wget -O splunk-10.0.0-e8eb0c4654f8-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/10.0.0/linux/splunk-10.0.0-e8eb0c4654f8-linux-amd64.deb"
 
-# Téléchargement
-wget "$URL"
 
 # Installation du paquet
 sudo dpkg -i splunk-${VERSION}-${BUILD}-Linux-x86_64.deb || sudo apt-get install -f -y
