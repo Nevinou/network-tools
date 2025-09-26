16c1330d7e8bbc4988ebd6357253192f276f18e9
Nevinou <neyhou@christophelouer.fr>
2025-09-26 11:42:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ REPO_BASE="https://raw.githubusercontent.com/Nevinou/network-tools/main"
 
 echo "[*] Installation des script réseau dans le répertoir $DOSSIER_INSTALL"
 
-curl "$REPO_BASE/$1">>$2
+curl "$REPO_BASE/$1">$2
 sudo chmod +x $2
 
 echo "[✔] Tous les outils ont été installés avec succès."
