12ba843e6ffb935df778b5126e53b3aef977dfd7
Nevinou <neyhou@christophelouer.fr>
2025-09-30 08:59:09 +0200

    Update rmDNS.sh

--- rmDNS.sh
+++ rmDNS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo rm /etc/bind/db.mon.lan
+sudo rm /etc/bind/db.b12.lan
+sudo rm /etc/bind/db.b13.lan
 sudo rm -r /var/cache/bind
 #sudo apt remove bind9 -y
 sudo apt purge bind9 -y 
